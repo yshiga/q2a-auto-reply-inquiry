5181726861e90fb8daed8cc9f4124d50531d2210
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-07-08 10:35:57 +0900

    Clean event file

--- qa-auto-reply-inquiry.php
+++ qa-auto-reply-inquiry.php
@@ -7,7 +7,6 @@ class qa_auto_reply_inquiry
 
 	function process_event($event, $userid, $handle, $cookieid, $params)
 	{
-
 		if ($event === 'feedback') {
 			if (isset($params['email'])) {
 				$ipaddress = qa_remote_ip_address();
@@ -16,10 +15,4 @@ class qa_auto_reply_inquiry
 			}
 		}
 	}
-	//
-	// private function get_oldquestion($postid = null)
-	// {
-	// 	$post = qa_db_single_select(qa_db_full_post_selectspec(null, $postid));
-	// 	return $post;
-	// }
 }
