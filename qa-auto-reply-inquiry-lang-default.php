6d79de52af001f034e7a98a06fce4bcab71e59aa
yaimapp <gmonday730-mbp@yahoo.co.jp>
2017-05-30 11:45:11 +0900

    言語ファイル追加

---
+++ qa-auto-reply-inquiry-lang-default.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+  'mail_subject' => '【^】お問い合わせありがとうございます（自動返信）',
+  'mail_body_header' => "お問い合わせありがとうございます。\n\n以下の内容でお問い合わせを受け付けました。このメールは自動送信されております。\n\n担当者からの返信をお待ち下さい。\n\nなお、お問い合わせから質問の投稿はできません。質問を投稿される方は、\nユーザー登録後に以下のページから質問を投稿してください。\n\n",
+  'mail_body_ask' => "質問投稿ページ：\n^\n\n",
+  'mail_body_naiyo' => "------- お問い合わせ内容 ------\n",
+  'mail_body_comment' => "コメント:\n^\n\n",
+  'mail_body_name' => "お名前:\n^\n\n",
+  'mail_body_email' => "メールアドレス:\n^\n\n",
+  'mail_body_previous' => "直前のページ:\n^\n\n",
+  'mail_body_ipaddress' => "IPアドレス:\n^\n\n",
+  'mail_body_browser' => "ブラウザ:\n^\n\n",
+);
